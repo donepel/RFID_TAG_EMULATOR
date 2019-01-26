d1608afb144278cd9bfd94100b4659e5e554a240
donepel <epel@epelbyte.com>
2019-01-26 19:13:35 -0300

    agrego archivos varios

---
+++ Notas_de_la_version.ino
@@ -0,0 +1,16 @@
+/******************************
+ * 
+ * 
+ * Version 0.2
+ * Fecha: 20-01-2019
+ * Descripion: Optimizacion del codigo y traduccion de comentarios
+ * datos: 1546 bytes
+ * ram: 137 bytes
+ * ***************************
+ * Version 0.1 
+ * Fecha: 26-01-2019
+ * Descripcion: puesta en marcha del circuito y codigo propuesto por sketchsk3tch
+ * en https://www.instructables.com/id/Stupid-Simple-Arduino-LF-RFID-Tag-Spoofer/
+ * datos: 1560 bytes
+ * ram: 137 bytes
+ */
