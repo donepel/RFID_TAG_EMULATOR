0c8f379a6110209fcd80c5748245f5c48d236ad7
donepel <epel@epelbyte.com>
2019-01-31 09:28:47 -0300

    version 0.3

--- Notas_de_la_version.ino
+++ Notas_de_la_version.ino
@@ -1,6 +1,11 @@
 /******************************
+ * Version 0.3
+ * Fecha: 29-01-2019
+ * Descripion: generacion de banco de tarjetas y ataque por diccionario
+ * datos: 2150 bytes
+ * ram: 521 bytes
  * 
- * 
+ * ****************************
  * Version 0.2
  * Fecha: 20-01-2019
  * Descripion: Optimizacion del codigo y traduccion de comentarios
